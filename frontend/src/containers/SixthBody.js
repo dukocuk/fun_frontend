d19f0df264c5b40fb6d993660f429e02ac360b35
Duran Köse <duk@netcompany.com>
2024-02-26 17:32:16 +0100

    updated code

--- frontend/src/containers/SixthBody.js
+++ frontend/src/containers/SixthBody.js
@@ -18,13 +18,16 @@ import { GiSettingsKnobs } from "react-icons/gi";
 import w1 from "../img/w1.png";
 import w2 from "../img/w2.png";
 import w3 from "../img/w3.png";
+import win1 from "../img/win1.png";
+import win2 from "../img/win2.png";
+import win3 from "../img/win3.png";
 import world from "../img/world.png";
 
 export default function SixthBody() {
   return (
     <Container
-      style={{ background: "#fffcf2", paddingTop: "160px" }}
-      fluid="true"
+      style={{ backgroundColor: "#fffcf2", paddingTop: "120px" }}
+      fluid
     >
       <Container>
         <Row
@@ -42,87 +45,107 @@ export default function SixthBody() {
               tellus. Et volutpat proin neque placerat at bibendum quam tellus.
             </p>
           </Col>
-          <Col>
-            <ListGroup variant="flush" style={{ width: "15%" }}>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <IoSearchOutline />
-                Bibendum tellus
-              </Button>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <GoShieldCheck />
-                Cras eget
-              </Button>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <IoRocketOutline />
-                Dolor pharetra
-              </Button>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <SlScreenDesktop />
-                Amet, fringilla
-              </Button>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <BiPodcast />
-                Amet nibh
-              </Button>
-              <Button className="btnB" style={{ textAlign: "left" }}>
-                <GiSettingsKnobs />
-                Sed velit
-              </Button>
-            </ListGroup>
-          </Col>
-          <Col>
-            <Image
-              src={w1}
-              style={{
-                width: "100%",
-                height: "35rem",
-                zIndex: 0,
-                transform: "rotate(0deg)",
-                top: 980,
-                left: 0,
-                position: "relative",
-              }}
-            />
-            <Image
-              src={w2}
-              style={{
-                width: "100%",
-                height: "35rem",
-                zIndex: 1,
-                transform: "rotate(0deg)",
-                top: 490,
-                left: 0,
-                position: "relative",
-              }}
-            />
-            <Image
-              src={w3}
-              style={{
-                width: "100%",
-                height: "35rem",
-                zIndex: 1,
-                transform: "rotate(0deg)",
-                top: 10,
-                left: 0,
-                position: "relative",
-              }}
-            />
+          <Col style={{ display: "flex", flexDirection: "column" }}>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <IoSearchOutline style={{ marginRight: "10px" }} />
+              Bibendum tellus
+            </Button>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <GoShieldCheck style={{ marginRight: "10px" }} />
+              Cras eget
+            </Button>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <IoRocketOutline style={{ marginRight: "10px" }} />
+              Dolor pharetra
+            </Button>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <SlScreenDesktop style={{ marginRight: "10px" }} />
+              Amet, fringilla
+            </Button>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <BiPodcast style={{ marginRight: "10px" }} />
+              Amet nibh
+            </Button>
+            <Button className="btnD" style={{ textAlign: "left" }}>
+              <GiSettingsKnobs style={{ marginRight: "10px" }} />
+              Sed velit
+            </Button>
           </Col>
         </Row>
-        <Container style={{ backgroundColor: "#78350f" }}>
-          <Image
-            src={world}
-            style={{
-              width: "100%",
-              height: "35rem",
-              zIndex: 1,
-              transform: "rotate(0deg)",
-              top: 0,
-              left: 0,
-              position: "relative",
-            }}
-          />
-        </Container>
+        <Image
+          src={win1}
+          style={{
+            width: "70%",
+            height: "35rem",
+            zIndex: 1,
+            transform: "rotate(0deg)",
+            top: -200,
+            left: 300,
+            position: "relative",
+          }}
+        />
+        <Image
+          src={win2}
+          style={{
+            width: "70%",
+            height: "35rem",
+            zIndex: 2,
+            transform: "rotate(0deg)",
+            top: -650,
+            left: 400,
+            position: "relative",
+          }}
+        />
+
+        <Image
+          src={w1}
+          style={{
+            width: "100%",
+            height: "35rem",
+            zIndex: 0,
+            transform: "rotate(0deg)",
+            top: -1200,
+            left: 0,
+            position: "relative",
+          }}
+        />
+        <Image
+          src={w2}
+          style={{
+            width: "100%",
+            height: "35rem",
+            zIndex: 1,
+            transform: "rotate(0deg)",
+            top: -1700,
+            left: 0,
+            position: "relative",
+          }}
+        />
+        <Image
+          src={w3}
+          style={{
+            width: "100%",
+            height: "35rem",
+            zIndex: 1,
+            transform: "rotate(0deg)",
+            top: -2200,
+            left: 0,
+            position: "relative",
+          }}
+        />
+        <Image
+          src={world}
+          style={{
+            width: "100%",
+            height: "35rem",
+            zIndex: 1,
+            transform: "rotate(0deg)",
+            top: -2200,
+            left: 0,
+            position: "relative",
+            backgroundColor: "#78350f",
+          }}
+        />
       </Container>
     </Container>
   );
