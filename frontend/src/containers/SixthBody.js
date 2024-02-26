a0ef9c71a35488480126d213c66d91e58ec21e68
Duran Köse <duk@netcompany.com>
2024-02-26 18:06:19 +0100

    updated code

--- frontend/src/containers/SixthBody.js
+++ frontend/src/containers/SixthBody.js
@@ -1,13 +1,4 @@
-import {
-  Container,
-  Row,
-  Col,
-  ListGroup,
-  Button,
-  Image,
-  Card,
-  CardBody,
-} from "react-bootstrap";
+import { Container, Button, Image } from "react-bootstrap";
 import { IoSearchOutline } from "react-icons/io5";
 import { GoShieldCheck } from "react-icons/go";
 import { IoRocketOutline } from "react-icons/io5";
@@ -15,13 +6,14 @@ import { SlScreenDesktop } from "react-icons/sl";
 import { BiPodcast } from "react-icons/bi";
 import { GiSettingsKnobs } from "react-icons/gi";
 
-import w1 from "../img/w1.png";
-import w2 from "../img/w2.png";
-import w3 from "../img/w3.png";
 import win1 from "../img/win1.png";
 import win2 from "../img/win2.png";
 import win3 from "../img/win3.png";
+import w1 from "../img/w1.png";
+import w2 from "../img/w2.png";
+import w3 from "../img/w3.png";
 import world from "../img/world.png";
+import num from "../img/num.png";
 
 export default function SixthBody() {
   return (
@@ -30,48 +22,39 @@ export default function SixthBody() {
       fluid
     >
       <Container>
-        <Row
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Col>
-            <h1 className="display-4">Grow your collection</h1>
-            <p>
-              Enim neque massa porta adipiscing elit. Sem libero id faucibus
-              nibh amet dictum pellentesque sed. Eu non turpis risus odio
-              sapien, fames sit rhoncus. Nec magna sed interdum sit purus
-              tellus. Et volutpat proin neque placerat at bibendum quam tellus.
-            </p>
-          </Col>
-          <Col style={{ display: "flex", flexDirection: "column" }}>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <IoSearchOutline style={{ marginRight: "10px" }} />
-              Bibendum tellus
-            </Button>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <GoShieldCheck style={{ marginRight: "10px" }} />
-              Cras eget
-            </Button>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <IoRocketOutline style={{ marginRight: "10px" }} />
-              Dolor pharetra
-            </Button>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <SlScreenDesktop style={{ marginRight: "10px" }} />
-              Amet, fringilla
-            </Button>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <BiPodcast style={{ marginRight: "10px" }} />
-              Amet nibh
-            </Button>
-            <Button className="btnD" style={{ textAlign: "left" }}>
-              <GiSettingsKnobs style={{ marginRight: "10px" }} />
-              Sed velit
-            </Button>
-          </Col>
-        </Row>
+        <h1 className="display-4">Grow your collection</h1>
+        <p>
+          Enim neque massa porta adipiscing elit. Sem libero id faucibus nibh
+          amet dictum pellentesque sed. Eu non turpis risus odio sapien, fames
+          sit rhoncus. Nec magna sed interdum sit purus tellus. Et volutpat
+          proin neque placerat at bibendum quam tellus.
+        </p>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <IoSearchOutline style={{ marginRight: "10px" }} />
+            Bibendum tellus
+          </Button>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <GoShieldCheck style={{ marginRight: "10px" }} />
+            Cras eget
+          </Button>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <IoRocketOutline style={{ marginRight: "10px" }} />
+            Dolor pharetra
+          </Button>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <SlScreenDesktop style={{ marginRight: "10px" }} />
+            Amet, fringilla
+          </Button>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <BiPodcast style={{ marginRight: "10px" }} />
+            Amet nibh
+          </Button>
+          <Button className="btnD" style={{ textAlign: "left" }}>
+            <GiSettingsKnobs style={{ marginRight: "10px" }} />
+            Sed velit
+          </Button>
+        </div>
         <Image
           src={win1}
           style={{
@@ -83,6 +66,7 @@ export default function SixthBody() {
             left: 300,
             position: "relative",
           }}
+          fluid
         />
         <Image
           src={win2}
@@ -95,8 +79,21 @@ export default function SixthBody() {
             left: 400,
             position: "relative",
           }}
+          fluid
         />
-
+        <Image
+          src={win3}
+          style={{
+            width: "30%",
+            height: "25rem",
+            zIndex: 2,
+            transform: "rotate(0deg)",
+            top: -650,
+            left: 120,
+            position: "relative",
+          }}
+          fluid
+        />
         <Image
           src={w1}
           style={{
@@ -108,6 +105,7 @@ export default function SixthBody() {
             left: 0,
             position: "relative",
           }}
+          fluid
         />
         <Image
           src={w2}
@@ -120,6 +118,7 @@ export default function SixthBody() {
             left: 0,
             position: "relative",
           }}
+          fluid
         />
         <Image
           src={w3}
@@ -132,6 +131,7 @@ export default function SixthBody() {
             left: 0,
             position: "relative",
           }}
+          fluid
         />
         <Image
           src={world}
@@ -145,6 +145,20 @@ export default function SixthBody() {
             position: "relative",
             backgroundColor: "#78350f",
           }}
+          fluid
+        />
+        <Image
+          src={num}
+          style={{
+            width: "100%",
+            height: "12rem",
+            zIndex: 1,
+            transform: "rotate(0deg)",
+            top: -2650,
+            left: 0,
+            position: "relative",
+          }}
+          fluid
         />
       </Container>
     </Container>
