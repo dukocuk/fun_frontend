9146ffca6241651a220f8f410c1231fe9bd457db
s147153 <s147153@student.dtu.dk>
2024-02-28 16:10:12 +0100

    fixed error

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button, Card } from "react-bootstrap";
 import { useState } from "react";
 import Quiz from "./containers/Quiz";
-import GoogleMapReact from "google-map-react";
 
 function useGeolocation() {}
 
